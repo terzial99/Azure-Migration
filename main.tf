eaa8760936232985d199bec9a61874df0faf2ee4
terzial <terzial99@gmail.com>
2026-09-13 10:06:02 -0400

    Add initial Azure Terraform configuration

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_resource_group" "test" {
+  name     = "rg-terraform-test"
+  location = "Canada Central"
+}resource "azurerm_resource_group" "test" {
+  name     = "rg-terraform-test"
+  location = "Canada Central"
+}
