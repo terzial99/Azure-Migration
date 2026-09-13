91c1d71ce1886416ba15a8a72a8dd6bd575de49e
terzial <terzial99@gmail.com>
2026-09-13 11:45:05 -0400

    Add Active Directory network security rules

--- main.tf
+++ main.tf
@@ -28,3 +28,30 @@ resource "azurerm_subnet_network_security_group_association" "workload" {
   subnet_id                 = module.vnet.subnets["workload"].resource_id
   network_security_group_id = azurerm_network_security_group.workload.id
 }
+resource "azurerm_network_security_rule" "ad_tcp" {
+  name                        = "Allow-AD-TCP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["53", "88", "135", "389", "445", "464", "636", "3268", "3269", "49152-65535"]
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = azurerm_resource_group.test.name
+  network_security_group_name = azurerm_network_security_group.workload.name
+}
+
+resource "azurerm_network_security_rule" "ad_udp" {
+  name                        = "Allow-AD-UDP"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Udp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["53", "88", "123", "389", "464"]
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = azurerm_resource_group.test.name
+  network_security_group_name = azurerm_network_security_group.workload.name
+}
