3c815b41faab56a3f92e9e8cf49527b7e96e8ebe
terzial <terzial99@gmail.com>
2026-09-13 11:36:06 -0400

    Add workload network security group

--- main.tf
+++ main.tf
@@ -19,3 +19,8 @@ module "vnet" {
     }
   }
 }
+resource "azurerm_network_security_group" "workload" {
+  name                = "nsg-workload"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
