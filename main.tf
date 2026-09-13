9c9dde458c6427bad0224a9b8798251acb96577a
terzial <terzial99@gmail.com>
2026-09-13 11:40:43 -0400

    Associate NSG with workload subnet

--- main.tf
+++ main.tf
@@ -24,3 +24,7 @@ resource "azurerm_network_security_group" "workload" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 }
+resource "azurerm_subnet_network_security_group_association" "workload" {
+  subnet_id                 = module.vnet.subnets["workload"].resource_id
+  network_security_group_id = azurerm_network_security_group.workload.id
+}
