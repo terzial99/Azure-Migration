98ada8d9c179c828be21b6a420ab3c88a14de775
terzial <terzial99@gmail.com>
2026-09-13 10:11:59 -0400

    Fix resource group configuration

--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
 resource "azurerm_resource_group" "test" {
   name     = "rg-terraform-test"
   location = "Canada Central"
-}resource "azurerm_resource_group" "test" {
-  name     = "rg-terraform-test"
-  location = "Canada Central"
 }
